75533ad8c966c9ab829102accbf9b76059f8a497
mayzer100500 <vladimir.gizhitskiy@gmail.com>
2023-08-15 20:40:51 +0300

    a little fix

--- index.js
+++ index.js
@@ -28,12 +28,8 @@ onValue(shoppingListInDB, function (snapshot) {
     if (snapshot.exists()) {
         let itemsArray = Object.entries(snapshot.val())
         clearShoppingListEl()
-    
         for (let i = 0; i < itemsArray.length; i++) {
             let currentItem = itemsArray[i]
-            let currentItemID = currentItem[0]
-            let currentItemValue = currentItem[1]
-        
             appendItemToShoppingListEl(currentItem)
         }
     } else {
@@ -52,18 +48,12 @@ function clearInputFieldEl() {
 function appendItemToShoppingListEl(item) {
     let itemID = item[0]
     let itemValue = item[1]
-    
     let newEl = document.createElement("li")
-    
     newEl.textContent = itemValue
     shoppingListEl.append(newEl)
-    
-    // Challenge: Attach an event listener to newEl and make it so you console log the id of the item when it's pressed.
     newEl.addEventListener("click", function() {
-        // Challenge: Make a let variable called 'exactLocationOfItemInDB' and set it equal to ref(database, something) where you substitute something with the code that will give you the exact location of the item in question.
         let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
         console.log(exactLocationOfItemInDB)
-        // Challenge: Use the remove function to remove the item from the database
         remove(exactLocationOfItemInDB)
     })
     
